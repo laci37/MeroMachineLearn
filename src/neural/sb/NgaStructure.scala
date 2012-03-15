615ded279b102a01dd8f3581f77cff9a093f4a53
Mérő László <laci37@gmail.com>
2012-03-15 20:24:50 +0100

    finished NgaStrucuture implementation

--- src/neural/sb/NgaStructure.scala
+++ src/neural/sb/NgaStructure.scala
@@ -2,10 +2,14 @@ package neural.sb
 import nga._
 import neural._
 import neural.nga._
-class NgaStructure(out:MutableLayer,test:(Net=>Double)) extends Structure(out) {
-  
-  override lazy val baseCost={
-    val initGenome=Iterable.fill(20)(WeightGenome.zero(out,test))
-    val gen=new GenerationBase(initGenome) with Elitism with StallCount
+class NgaStructure(out: MutableLayer, test: (Net => Double)) extends Structure(out) {
+  var maxStall = 50
+  var genSize = 20
+  override lazy val baseCost = {
+    val initGenome = Iterable.fill(genSize)(WeightGenome.zero(out, test))
+    val gen = new GenerationBase(initGenome) with Elitism with StallCount
+    gen.resetBest = false
+    while (gen.stall < maxStall) gen.step()
+    gen.best.fitness
   }
 }
