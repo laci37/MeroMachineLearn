5e75cb5e2a437df46fad1321441110b69bae8f1a
Mérő László <laci37@gmail.com>
2012-05-25 14:05:52 +0200

    test code changes

--- src/test/plot/Init.scala
+++ src/test/plot/Init.scala
@@ -2,7 +2,7 @@ package test.plot
 import util.plot._
 import java.awt._
 class Init {
-  val plot=new TimePlotter((Color.red,new BasicStroke(2)),
-		  					  (Color.blue,new BasicStroke(4)))
-  val wnd=new TimePlotFrame(plot)
+  val plot = new TimePlotter((Color.red, new BasicStroke(2)),
+    (Color.blue, new BasicStroke(4)))
+  val wnd = new TimePlotFrame(plot)
 }
--- src/test/simrot/Init.scala
+++ src/test/simrot/Init.scala
@@ -1,23 +1,21 @@
 package test.simrot
 import util.plot._
 import control.rotator._
-import SimulatedRotator._
+import SimObject._
 import java.awt._
-class Init {
+class Init extends {
   val stroke = new BasicStroke(1)
-  val plot = new TimePlotter((Color.blue, stroke), (Color.red, stroke), (Color.black, stroke))
-  val wnd= new TimePlotFrame(plot)
-  var red = 1d
-  var blue = -1d
-  var rot=new SimulatedRotator(1,1)
-  rot.torques = rot.torques :+ spring(1,0) _
-  rot.dynFriction=0.49
-  rot.stcFriction=0.5
-  
-  def calc()={
-   for(i<-(1 to 100)){
-     rot.timestep(0.1)
-     plot.step(0.1,red,blue,rot.pos)
-   } 
+  val plot = new TimePlotter((Color.blue, stroke), (Color.red, stroke))
+  val wnd = new TimePlotFrame(plot)
+  var rot = new SimulatedRotator(1, 1)
+  rot.objects = Seq(new ExcentricLoad(1, 1))
+  rot.dynFriction = 0.49
+  rot.stcFriction = 0.5
+
+  def calc() = {
+    for (i <- (1 to 1000)) {
+      rot.timestep(0.01)
+      plot.step(0.01, rot.vel, rot.pos)
+    }
   }
 }
