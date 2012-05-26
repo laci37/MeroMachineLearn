d64739e5cdb7fff347df5c5e7f462e07168b66ab
Mérő László <laci37@gmail.com>
2012-05-26 12:16:33 +0200

    changed neural.sb.Structure to have symmetrical neighbor operations

--- src/neural/dsl/Layer.scala
+++ src/neural/dsl/Layer.scala
@@ -88,5 +88,6 @@ object Layer {
     inner(out)
     ret
   }
+  
 
 }
--- src/neural/sb/Structure.scala
+++ src/neural/sb/Structure.scala
@@ -6,6 +6,11 @@ import annotation.tailrec
 abstract class Structure(val out: MutableLayer) extends State {
   def layers = _layers
   private var _layers = Layer.discover(out)
+  val input = layers.find(l => l.inputs.isEmpty).get
+  protected def mutableLayers =
+    for (l <- layers if (l.isInstanceOf[MutableLayer] && !l.const)) yield l.asInstanceOf[MutableLayer]
+  protected def connections =
+    (for (l <- layers) yield (for (il <- l.inputs) yield (l, il))).flatten
   val rand = scala.util.Random
   def baseCost(): Double
 
@@ -23,10 +28,12 @@ abstract class Structure(val out: MutableLayer) extends State {
   def neighbor(): State = {
     val r = rand.nextDouble()
     r match {
-      case a if a < 0.20 => mutActFunc
-      case a if a < 0.40 => addNeuron
-      case a if a < 0.60 => insertLayer
-      case a if a < 0.80 => removeNeuron
+      case a if a < 1/7d => mutActFunc
+      case a if a < 2/7d => addNeuron
+      case a if a < 3/7d => insertLayer
+      case a if a < 4/7d => removeNeuron
+      case a if a < 5/7d => removeLayer
+      case a if a < 6/7d => removeConnection
       case _ => addConnection
     }
   }
@@ -38,6 +45,7 @@ abstract class Structure(val out: MutableLayer) extends State {
   def addConnection = clone.asInstanceOf[Structure]._addConnection
   def removeNeuron = clone.asInstanceOf[Structure]._removeNeuron
   def removeLayer = clone.asInstanceOf[Structure]._removeLayer
+  def removeConnection = clone.asInstanceOf[Structure]._removeConnection
 
   //mutable state changes
   protected def _mutActFunc: Structure = {
@@ -71,24 +79,49 @@ abstract class Structure(val out: MutableLayer) extends State {
 
   protected def _removeLayer() = {
     val toRemove = randomMutableLayer
-    val outputs = for(l<-layers if(l.inputs.contains(toRemove))) yield l
-    outputs foreach {ol=>
+    val outputs = for (l <- layers if (l.inputs.contains(toRemove))) yield l
+    outputs foreach { ol =>
       ol.removeInput(toRemove)
-      toRemove.inputs foreach {il=>
-        il >> ol  
+      toRemove.inputs foreach { il =>
+        il >> ol
       }
     }
-    _layers= _layers diff Seq(toRemove)
+    _layers = _layers diff Seq(toRemove)
+    this
   }
+
+  protected def _removeConnection() = {
+    var conns = connections
+    @tailrec def inner(): Unit = {
+      val toRemove = conns(rand.nextInt(conns.length))
+      toRemove._1.inputs = toRemove._1.inputs diff Seq(toRemove._2)
+      if (checkPath) return
+      conns = conns diff Seq(toRemove)
+      toRemove._1.inputs = toRemove._1.inputs :+ toRemove._2
+      inner
+    }
+    inner
+    this
+  }
+
+  protected def checkPath(): Boolean = checkPath(out)
+
+  protected def checkPath(l: Layer): Boolean = {
+    if (l == input) return true
+    l.inputs foreach { il =>
+      if (il == input) return true
+      if (checkPath(il)) return true
+    }
+    return false
+  }
+  
 
   protected def randomLayer = {
     layers(rand.nextInt(layers.length))
   }
 
-  @tailrec protected final def randomMutableLayer: MutableLayer = {
-    val randlayer = randomLayer
-    if (randlayer.isInstanceOf[MutableLayer] && !randlayer.const) return randlayer.asInstanceOf[MutableLayer]
-    randomMutableLayer
+  protected def randomMutableLayer: MutableLayer = {
+    mutableLayers(rand.nextInt(mutableLayers.length))
   }
 
 }
