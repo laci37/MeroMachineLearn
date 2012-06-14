09382d22743d8b2c4280049bfd66f3f5b237ae69
Mérő László <laci37@gmail.com>
2012-06-14 15:02:40 +0200

    added calc(Int) to neural.Net

--- src/neural/Net.scala
+++ src/neural/Net.scala
@@ -42,6 +42,24 @@ trait Net extends Serializable {
     inner(Change)
   }
   
+  /**
+   * calculates a stable output, with at most cycles timesteps
+   * returns true if no overflow happens
+   */
+  def calc(cycles:Int):Boolean ={
+     import TimestepReturns._
+    @tailrec def inner(t: TimestepReturns,c:Int): Boolean = {
+      if(c<=0) return true
+      t match {
+        case Change => Unit
+        case NoChange => return true
+        case Overflow => return false
+      }
+      inner(timestep(),c-1)
+    }
+    inner(Change,cycles)
+  }
+  
   def reset() = {
     neurons foreach { n => n.reset }
   }
