26e8d93435bf28a049fe724c300ee7d31f6be4ff
Mérő László <laci37@gmail.com>
2012-07-02 19:37:32 +0200

    even more comments'

--- src/test/xornga/Init.scala
+++ src/test/xornga/Init.scala
@@ -1,44 +1,60 @@
-package xornga
+package test.xornga
 import nga._
 import neural._
 import neural.dsl._
 import neural.nga._
-object Init {
+import test._
+object Init extends TestBase {
+  val plan = (2 lin) >> (2 tanh) >> (1 tanh)
+  val gen = new GenerationBase(for (i <- 1 to 20) yield WeightGenome.zero(plan, test)) with Elitism //with TriggeredHypermutation
+  var c = 0
 
-  def main(args: Array[String]): Unit = {
-    val plan = (2 lin) >> (2 tanh) >> (1 tanh)
-    val gen = new GenerationBase(for (i <- 1 to 20) yield WeightGenome.zero(plan, test)) 
-       with Elitism //with TriggeredHypermutation
-    var c = 0
-    println("xorngastart")
-    println(System.currentTimeMillis())
-    while (gen.best.fitness < 1 && c < 1000000) {
-      gen.step()
-      println(gen.best.fitness)
-      c += 1
-    }
-    println(c)
-    println(System.currentTimeMillis())
+  def calc() = { 
+    gen.step
+    c += 1
+    print(".")
+    //stop if score is good or if number of cycles is exceeded
+    gen.best.fitness >= 0.9999 || c > 100000
   }
 
+  def interact(s:String) = {
+    println()
+    try { //interactive testing
+      val n = gen.best.asInstanceOf[WeightGenome].decode //create network
+      //set inputs
+      println("first input?")
+      n.inputs(0) set Console.readDouble()
+      println("second input?")
+      n.inputs(1) set Console.readDouble()
+      
+      n.calc() //calculate first stable output
+      print("output: ") // print it
+      println(n.outputs(0).output)
+    }
+    false //program never stops on its own
+  }
+
+  //problem definition
   def test(n: Net): Double = {
-    var score = 0d
-    n.inputs(0) set 0d
-    n.inputs(1) set 0d
-    n.calc()
-    score += math.pow(n.outputs(0).output + 1d, 2)
-    n.inputs(0) set 0d
+    var sqerrsum = 0d //squared error sum
+    n.inputs(0) set -1d
+    n.inputs(1) set -1d
+    n.calc() 
+    sqerrsum += math.pow(n.outputs(0).output + 1d, 2)
+    n.inputs(0) set -1d
     n.inputs(1) set 1d
     n.calc()
-    score += math.pow(n.outputs(0).output - 1d, 2)
+    sqerrsum += math.pow(n.outputs(0).output - 1d, 2)
     n.inputs(0) set 1d
-    n.inputs(1) set 0d
+    n.inputs(1) set -1d
     n.calc()
-    score += math.pow(n.outputs(0).output - 1d, 2)
+    sqerrsum += math.pow(n.outputs(0).output - 1d, 2)
     n.inputs(0) set 1d
     n.inputs(1) set 1d
     n.calc()
-    score += math.pow(n.outputs(0).output + 1d, 2)
-    if (!(score.isNaN || score.isInfinity)) math.pow(2, -score) else 0d
+    sqerrsum += math.pow(n.outputs(0).output + 1d, 2)
+    //if the network produces Nan-s it will have no chance to reproduce
+    //we transform the scores using an exponential function
+    if (!(sqerrsum.isNaN || sqerrsum.isInfinity)) math.pow(2, -sqerrsum) else 0d
   }
 }
